ce4c7256f5aca12241741593f14ba6a8b639dd3e
CRSampaio <c.r.sampaio@outlook.com>
2023-03-01 01:46:09 -0300

    It works!

--- src/FileReader.c
+++ src/FileReader.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 
-int main()
+void readFile(char *fileName);
+
+void main()
 {
-    printf("Hello World!\n");
-    /* code */
-    return 0;
+    printf("Initiliazing FileReader...\n");
+
+    readFile("src/resources/testFile.txt");
+
+    printf("Finishing FileReader...\n");
+}
+
+void readFile(char *fileName)
+{
+    FILE *file;
+
+    file = fopen(fileName, "r");
+
+    int characterValue;
+    
+    if (file == NULL)
+    {
+        perror("Error! File not found");
+        return;
+    }
+
+    while ((characterValue = fgetc(file)) != EOF)
+    {
+        // 0x80 = 128
+        // Here, we checking if the
+        if ((characterValue & 0x80) != 0x00)
+        {
+            printf("Character using more than one byte to be represented found. Char: %c\n",(char) characterValue);
+            break;
+        }
+
+        printf("Character using just one byte to be represented. Char: %c\n", (char) characterValue);
+    }
+
+    fclose(file);
+    
 }
